0764af3f36e113e06e25182bd66766b52d7ce581
Francesco <francesco.varotto.2@studenti.unipd.it>
2020-05-05 11:53:22 +0200

    new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/it/unipd/dei/esp1920/quickynews/MainActivity.java 	new file:   build.gradle Aggiunti file per progetto

---
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/MainActivity.java
@@ -0,0 +1,14 @@
+package it.unipd.dei.esp1920.quickynews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
