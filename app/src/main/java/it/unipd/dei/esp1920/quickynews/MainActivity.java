b57a53ee238f5b7198e36abd00c6e0ec996e6b86
francevarotz98 <46248324+francevarotz98@users.noreply.github.com>
2020-05-07 21:31:45 +0200

    created NetConnectionReceiver file-----> not finished yet

--- app/src/main/java/it/unipd/dei/esp1920/quickynews/MainActivity.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,21 +17,29 @@ import it.unipd.dei.esp1920.quickynews.fragments.Location;
 import it.unipd.dei.esp1920.quickynews.fragments.Settings;
 import it.unipd.dei.esp1920.quickynews.fragments.TopNews;
 
+import it.unipd.dei.esp1920.quickynews.connections.NetConnectionReceiver;
+
 public class MainActivity extends AppCompatActivity {
 
-    private int  firstaccess=0;                 //variabile che è sempre =1 tranne al primo accesso che è =0
+    private final String TAG="MainActivity";
+    private int  firstaccess=0; //variabile che è sempre =1 tranne al primo accesso che è =0
+    private BroadcastReceiver mNetConnectionReceiver;
     //easy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate()");
 
         BottomNavigationView botNav=findViewById(R.id.bottom_navigation);
         botNav.setOnNavigationItemSelectedListener(navList);
         // OGNI VOLTA CHE SI APRE L'APP VIENE FUORI IL FRAGMENT DELLE TOP NEWS...
         //NEL CASO DEL PRIMO ACCESSO METTEREI UN IF CHE MI PORTA ALLE SETTINGS
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_con, new TopNews()).commit();      //OGNI VOLTA CHE APRO L'APP ESCE TOP NEWS
+        mNetConnectionReceiver=new NetConnectionReceiver();
+
     }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navList= new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -54,4 +65,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+
+
 }
