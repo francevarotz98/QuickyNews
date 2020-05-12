9797d32f19206bbf6e819e81979e86f45fdee743
Pietro Albertini <pietro.albertini46@gmail.com>
2020-05-12 22:19:15 +0200

    Mbytes can be chosen

--- app/src/main/java/it/unipd/dei/esp1920/quickynews/Settings.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/Settings.java
@@ -1,11 +1,8 @@
 package it.unipd.dei.esp1920.quickynews;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,7 +10,7 @@ import it.unipd.dei.esp1920.quickynews.storage.AvailableSpace;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 
 
 public class Settings extends AppCompatActivity {
@@ -21,23 +18,52 @@ public class Settings extends AppCompatActivity {
 
     private SeekBar  mSeekBar;
     private TextView mTextView;
-    private long freeSpace;
+    private Toolbar mToolbar;
+    private int int_sb;
+    private String str_et;
+    private TextView mTextViewMin, mTextViewMax;        //textView da Parsare a int, da mettere a fianco
+                                                        // alla percentuale della seekbar
+    private long totSpace,minSpace,maxSpace;            //serve per settare il testo agli estremi della seekbar
+    private int mBChosen;                               //mBytes scelti dall'utente
     private static String TAG="SETTINGS";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"onCreate()");
-        freeSpace=AvailableSpace.getTotalDiskSpace();
-        setContentView(R.layout.settings_toolbar);
-        mTextView=(TextView)findViewById(R.id.sk_progression);
+        setContentView(R.layout.settings_main);
+
+        //TOOLBAR
+        mToolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(mToolbar);              //rende predefinata la toolbar creata
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        int_sb = preferences.getInt("seekBarValue", 0);
+        str_et = preferences.getString("editTextValue", null);
+
+        mTextView=(TextView)findViewById(R.id.tV_progression_percentage);
+        mTextView.setText(str_et);
         mSeekBar=(SeekBar)findViewById(R.id.sk_seekBar);
+        mSeekBar.setProgress(int_sb);
+
+        totSpace = AvailableSpace.getTotalDiskSpace();    //spazio totale
+
+        minSpace=(totSpace/100*1)/(1000000);    //min =1% del tot
+        maxSpace=(totSpace/100*7)/(1000000);   ///max=7%del tot
+
+        mTextViewMin=(TextView)findViewById(R.id.tV_minSpace);
+        mTextViewMax=(TextView)findViewById(R.id.tV_maxSpace);
+        mTextViewMax.setText("Max: " + maxSpace +" Mb");                    //serve solo all'aspetto grafico
+        mTextViewMin.setText("Min: " + minSpace+ " Mb");                    //serve solo all'aspetto grafico
 
 
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mTextView.setText("" + progress + "%");                             //mostra la percentuale di progresso
+                int tmp1 = (int)maxSpace;
+                int tmp2 = (int)minSpace;
+                mBChosen=(tmp1-tmp2)*progress/100+tmp2;
+                mTextView.setText("" + mBChosen + "Mb");                            //mostra la percentuale in Mb di progresso
                                                                                     // della seekbar
             }
 
@@ -51,5 +77,26 @@ public class Settings extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Store values between instances here
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        SeekBar mSeekBar = (SeekBar)findViewById(R.id.sk_seekBar);
+        int_sb = mSeekBar.getProgress();
+        editor.putInt("seekBarValue", int_sb);
+
+        mTextView= (TextView) findViewById(R.id.tV_progression_percentage);
+        str_et = mTextView.getText().toString();
+        editor.putString("editTextValue", str_et);
+
+        // Commit to storage synchronously
+        editor.commit();
     }
 }
