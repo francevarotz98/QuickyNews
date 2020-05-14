3f81e5c8589bf7e92a2dba5b0c4882937f476f6f
Pietro Albertini <pietro.albertini46@gmail.com>
2020-05-14 22:41:08 +0200

    Categories moved in Settings

--- app/src/main/java/it/unipd/dei/esp1920/quickynews/Settings.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/Settings.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -24,6 +25,7 @@ public class Settings extends AppCompatActivity {
     private Toolbar mToolbar;
     private int int_sb;
     private String str_et;
+    private boolean bln_cb_sport,bln_cb_tech,bln_cb_food,bln_cb_mot,bln_cb_econ,bln_cb_pol;         //variabili per salvare lo stato delle categorie
     private TextView mTextViewMin, mTextViewMax;        //textView da Parsare a int, da mettere a fianco
                                                         // alla percentuale della seekbar
     private long totSpace,minSpace,maxSpace;            //serve per settare il testo agli estremi della seekbar
@@ -47,6 +49,24 @@ public class Settings extends AppCompatActivity {
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         int_sb = preferences.getInt("seekBarValue", 0);
         str_et = preferences.getString("editTextValue", null);
+        bln_cb_sport = preferences.getBoolean("chBoxSport",false);
+        bln_cb_tech = preferences.getBoolean("chBoxTech",false);
+        bln_cb_food = preferences.getBoolean("chBoxFood",false);
+        bln_cb_mot = preferences.getBoolean("chBoxMot",false);
+        bln_cb_econ = preferences.getBoolean("chBoxEcon",false);
+        bln_cb_pol = preferences.getBoolean("chBoxPol",false);
+        CheckBox mSelectedSport=(CheckBox)findViewById(R.id.checkBox_sport);
+        CheckBox mSelectedTech=(CheckBox)findViewById(R.id.checkBox_tech);
+        CheckBox mSelectedFood=(CheckBox)findViewById(R.id.checkBox_food);
+        CheckBox mSelectedMot=(CheckBox)findViewById(R.id.checkBox_mot);
+        CheckBox mSelectedEcon=(CheckBox)findViewById(R.id.checkBox_econ);
+        CheckBox mSelectedPol=(CheckBox)findViewById(R.id.checkBox_pol);
+        mSelectedSport.setChecked(bln_cb_sport);
+        mSelectedTech.setChecked(bln_cb_tech);
+        mSelectedFood.setChecked(bln_cb_food);
+        mSelectedMot.setChecked(bln_cb_mot);
+        mSelectedEcon.setChecked(bln_cb_econ);
+        mSelectedPol.setChecked(bln_cb_pol);
 
         mTextView=(TextView)findViewById(R.id.tV_progression_percentage);
         mTextView.setText(str_et);
@@ -106,6 +126,27 @@ public class Settings extends AppCompatActivity {
         mTextView= (TextView) findViewById(R.id.tV_progression_percentage);
         str_et = mTextView.getText().toString();
         editor.putString("editTextValue", str_et);
+
+        //------SALVATAGGIO STATO
+        CheckBox mSelectedSport=(CheckBox)findViewById(R.id.checkBox_sport);
+        CheckBox mSelectedTech=(CheckBox)findViewById(R.id.checkBox_tech);
+        CheckBox mSelectedFood=(CheckBox)findViewById(R.id.checkBox_food);
+        CheckBox mSelectedMot=(CheckBox)findViewById(R.id.checkBox_mot);
+        CheckBox mSelectedEcon=(CheckBox)findViewById(R.id.checkBox_econ);
+        CheckBox mSelectedPol=(CheckBox)findViewById(R.id.checkBox_pol);
+        bln_cb_sport=mSelectedSport.isChecked();
+        bln_cb_tech=mSelectedTech.isChecked();
+        bln_cb_food=mSelectedFood.isChecked();
+        bln_cb_mot=mSelectedMot.isChecked();
+        bln_cb_econ=mSelectedEcon.isChecked();
+        bln_cb_pol=mSelectedPol.isChecked();
+
+        editor.putBoolean("chBoxSport", bln_cb_sport);
+        editor.putBoolean("chBoxTech", bln_cb_tech);
+        editor.putBoolean("chBoxFood", bln_cb_food);
+        editor.putBoolean("chBoxMot", bln_cb_mot);
+        editor.putBoolean("chBoxEcon", bln_cb_econ);
+        editor.putBoolean("chBoxPol", bln_cb_pol);
 
         // Commit to storage synchronously
         editor.commit();
--- app/src/main/java/it/unipd/dei/esp1920/quickynews/fragments/Categories.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/fragments/Categories.java
@@ -1,20 +1,15 @@
 package it.unipd.dei.esp1920.quickynews.fragments;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import it.unipd.dei.esp1920.quickynews.MainActivity;
 import it.unipd.dei.esp1920.quickynews.R;
 
 public class Categories extends Fragment {
@@ -28,28 +23,7 @@ public class Categories extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         Log.d(TAG,"onCreateView()");
-        View v = inflater.inflate(R.layout.fragment_categories,container,false);
-
-        //------------PER SALVATAGGIO STATO---------
-        SharedPreferences preferences = this.getActivity().getPreferences(Context.MODE_PRIVATE);
-        bln_cb_sport = preferences.getBoolean("chBoxSport",false);
-        bln_cb_tech = preferences.getBoolean("chBoxTech",false);
-        bln_cb_food = preferences.getBoolean("chBoxFood",false);
-        bln_cb_mot = preferences.getBoolean("chBoxMot",false);
-        bln_cb_econ = preferences.getBoolean("chBoxEcon",false);
-        bln_cb_pol = preferences.getBoolean("chBoxPol",false);
-        CheckBox mSelectedSport=(CheckBox) v.findViewById(R.id.checkBox_sport);
-        CheckBox mSelectedTech=(CheckBox) v.findViewById(R.id.checkBox_tech);
-        CheckBox mSelectedFood=(CheckBox) v.findViewById(R.id.checkBox_food);
-        CheckBox mSelectedMot=(CheckBox) v.findViewById(R.id.checkBox_mot);
-        CheckBox mSelectedEcon=(CheckBox) v.findViewById(R.id.checkBox_econ);
-        CheckBox mSelectedPol=(CheckBox) v.findViewById(R.id.checkBox_pol);
-        mSelectedSport.setChecked(bln_cb_sport);
-        mSelectedTech.setChecked(bln_cb_tech);
-        mSelectedFood.setChecked(bln_cb_food);
-        mSelectedMot.setChecked(bln_cb_mot);
-        mSelectedEcon.setChecked(bln_cb_econ);
-        mSelectedPol.setChecked(bln_cb_pol);
+        View v = inflater.inflate(R.layout.fragment_for_you,container,false);
         return v;
     }
 
@@ -57,31 +31,6 @@ public class Categories extends Fragment {
     public void onPause() {
         super.onPause();
         Log.d(TAG,"onPause() ");
-
-        SharedPreferences preferences = this.getActivity().getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        //------SALVATAGGIO STATO
-        CheckBox mSelectedSport=(CheckBox)getView().findViewById(R.id.checkBox_sport);
-        CheckBox mSelectedTech=(CheckBox)getView().findViewById(R.id.checkBox_tech);
-        CheckBox mSelectedFood=(CheckBox)getView().findViewById(R.id.checkBox_food);
-        CheckBox mSelectedMot=(CheckBox)getView().findViewById(R.id.checkBox_mot);
-        CheckBox mSelectedEcon=(CheckBox)getView().findViewById(R.id.checkBox_econ);
-        CheckBox mSelectedPol=(CheckBox)getView().findViewById(R.id.checkBox_pol);
-        bln_cb_sport=mSelectedSport.isChecked();
-        bln_cb_tech=mSelectedTech.isChecked();
-        bln_cb_food=mSelectedFood.isChecked();
-        bln_cb_mot=mSelectedMot.isChecked();
-        bln_cb_econ=mSelectedEcon.isChecked();
-        bln_cb_pol=mSelectedPol.isChecked();
-
-        editor.putBoolean("chBoxSport", bln_cb_sport);
-        editor.putBoolean("chBoxTech", bln_cb_tech);
-        editor.putBoolean("chBoxFood", bln_cb_food);
-        editor.putBoolean("chBoxMot", bln_cb_mot);
-        editor.putBoolean("chBoxEcon", bln_cb_econ);
-        editor.putBoolean("chBoxPol", bln_cb_pol);
-        editor.commit();
 
     }
 
