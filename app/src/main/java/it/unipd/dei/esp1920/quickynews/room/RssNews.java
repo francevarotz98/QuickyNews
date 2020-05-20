4c080cc009c68bb3c460bc80d546c1fa236d0c34
Francesco <francesco.varotto.2@studenti.unipd.it>
2020-05-20 18:28:17 +0200

    Started with Room db

---
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/room/RssNews.java
@@ -0,0 +1,67 @@
+package it.unipd.dei.esp1920.quickynews.room;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "rssnews_table")
+public class RssNews
+{
+
+    @PrimaryKey(autoGenerate = true) //autogenerate primary keys
+    @NonNull
+    private int id_Rss;
+    private String URL;
+    private String title;
+    private String description;
+    private String date;
+    private String category;
+    //private final String  TAG="RssNews";
+
+    public RssNews(@NonNull int id_Rss, String URL,String title,String description,String date,String category){
+        this.id_Rss=id_Rss;
+        this.URL=URL;
+        this.title=title;
+        this.description=description;
+        this.date=date;
+        this.category=category;
+    }
+
+    @NonNull
+    public int getId_Rss(){
+        //Log.d("RssNews","getId_Rss()");
+        return this.id_Rss;
+    }
+
+    public String getURL(){
+        //Log.d("RssNews","getURL()");
+        return this.URL;
+    }
+
+    public String getTitle(){
+       // Log.d("RssNews","getTitle()");
+        return this.title;
+    }
+
+    public String getDescription(){
+       // Log.d("RssNews","getDescription()");
+        return this.description;
+    }
+
+    public String getDate(){
+       // Log.d("RssNews","getDate()");
+        return this.date;
+    }
+
+    public String getCategory(){
+       // Log.d("RssNews","getCategory()");
+        return this.category;
+    }
+
+
+
+}
