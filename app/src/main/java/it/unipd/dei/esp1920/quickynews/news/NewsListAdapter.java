21ee0f620bd10551ddf5ef38bb559dea3a713e5b
Pietro Albertini <pietro.albertini46@gmail.com>
2020-05-29 18:54:06 +0200

    Toast appears when articles are long-pressed.

--- app/src/main/java/it/unipd/dei/esp1920/quickynews/news/NewsListAdapter.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/news/NewsListAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -112,15 +113,15 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.Articl
             }
         });
 
-/*
-        holder.mView.setOnLongClickListener(new View.OnLongClickListener(){
-
+        holder.mView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onLongCLick(View v){
-
+            public boolean onLongClick(View v) {
+                Context context = v.getContext();
+                Toast toast =Toast.makeText(context,"Bella raga! 130",Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
             }
         });
-*/
     }
 
     public void setArticle(List<Article> articles){
