6e8dac24b9071bb21d5ebdf5c4ffe9c52139876e
Giovanni Zanocco <giovanni@zanocco.it>
2020-06-09 22:39:30 +0200

    Migliorata visualizzazione WebView

--- app/src/main/java/it/unipd/dei/esp1920/quickynews/connections/NetConnectionReceiver.java
+++ app/src/main/java/it/unipd/dei/esp1920/quickynews/connections/NetConnectionReceiver.java
@@ -47,6 +47,12 @@ public class NetConnectionReceiver extends BroadcastReceiver {
         }
     }
 
+    public static boolean isMetered(Context context) {
+        Log.d(TAG,"isMetered()");
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.isActiveNetworkMetered();
+    }
+
     public static boolean isConnected(Context context) {
         Log.d(TAG,"isConnected()");
         checkConnection(context);
